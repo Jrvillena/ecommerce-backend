29d4247fa16dcf97c9172d1c7a85f58d1f2e3aaa
justin <justinrvillena@gmail.com>
2021-06-16 22:05:53 -0700

    added foreeign keys

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(category, {
+  foreignKey: 'category_id'
+});
+
 
 // Categories have many Products
+Category.hasMany(product, {
+  foreignKey: 'category_id',
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+through: ProductTag,
+foreignKey: 'tag_id'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+  });
+
 
 module.exports = {
   Product,
